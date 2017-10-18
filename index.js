e33a3e66efb027dbdf82c15db6a0e5471bfa51f4
Grégory Le Garec <gregory.legarec@gmail.com>
2017-10-18 15:01:34 +0200

    fix: remove generated error 🔥

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 'use strict'
 
-const {BaseKonnector, log} = require('cozy-konnector-libs')
+const {BaseKonnector} = require('cozy-konnector-libs')
 
 module.exports = new BaseKonnector(fields => {
   const timeout = Number(fields.timeout) || 8000
   if (timeout > 0) {
     return new Promise(resolve => {
       setTimeout(() => {
-        generateError(fields.error)
         resolve()
       }, timeout)
     })
   }
-
-  generateError(fields.error)
 })
-
-function generateError (error) {
-  if (error !== 'OK') log('error', error)
-}
